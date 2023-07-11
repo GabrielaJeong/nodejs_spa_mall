7993923af8976959f2f638d2774bc7e1b9db3d72
GabrielaJeong <fbrkqls94@gmail.com>
2023-07-12 02:59:47 +0900

    additional encoding

--- routes/post.js
+++ routes/post.js
@@ -1,18 +1,55 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
-
+const Joi = require('joi');
+const cookieParser = require('cookie-parser');
 const Post = require('../models/post');
 
-// 게시글 작성
-router.post('/', async (req, res) => {
-    const { title, password, user, content } = req.body;
+router.use(CookieParser())
+
+
+const postSchema = Joi.object({
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+});
+
+// JWT 미들웨어 - 쿠키 확인
+const verifyToken = (req, res, next) => {
     try {
-        const savedPost = await Post.create({ title, password, user, content });
+        // 쿠키에서 토큰을 가져옵니다.
+        const tokenValue = req.cookies.user;
+        if (!tokenValue) {
+            res.status(403).json({ errorMessage: '로그인이 필요한 기능입니다.' });
+            return;
+        }
+        const { userId } = jwt.verify(tokenValue, process.env.JWT_SECRET_KEY);
+        res.locals.userId = userId;
+        next();
+    } catch (error) {
+        res.status(403).json({ errorMessage: '전달된 쿠키에서 오류가 발생하였습니다.' });
+    }
+};
+
+// 게시글 작성
+router.post('/', verifyToken, async (req, res) => {
+    try {
+        const { error } = postSchema.validate(req.body);
+        if (error) {
+            if (error.details[0].context.key === 'title') {
+                return res.status(412).send({ errorMessage: '게시글 제목의 형식이 일치하지 않습니다.' });
+            } else if (error.details[0].context.key === 'content') {
+                return res.status(412).send({ errorMessage: '게시글 내용의 형식이 일치하지 않습니다.' });
+            }
+        }
+
+        const { title, content } = req.body;
+        const userId = res.locals.userId;
+
+        const savedPost = await Post.create({ title, content, userId });
         res.status(201).json({ message: "게시글을 생성하였습니다." });
     } catch (error) {
-        console.log(error)
-        res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
-
+        console.log(error);
+        res.status(400).json({ errorMessage: '게시글 작성에 실패하였습니다.' });
     }
 });
 
@@ -22,10 +59,10 @@ router.get('/', async (req, res) => {
         const posts = await Post.find().sort('-createdAt');
         res.json(posts);
     } catch (error) {
-        res.status(400).json({ error: error.toString() });
+        console.log(error);
+        res.status(400).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
     }
 });
-
 
 
 // 게시글 상세조회 (특정 게시글)
@@ -33,26 +70,42 @@ router.get('/:_postId', async (req, res) => {
     try {
         const post = await Post.findById(req.params._postId);
         if (!post) {
-            return res.status(404).json({ error: "포스트를 찾을 수 없습니다."});
+            return res.status(404).json({ errorMessage: '포스트를 찾을 수 없습니다.' });
         }
         res.json(post);
     } catch (error) {
         console.log(error);
-        res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+        res.status(400).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
     }
 });
 
 
+
 // 게시글 수정
 router.put('/:_postId', async (req, res) => {
-    const { password, ...rest } = req.body;
+    const { title, content, password, ...rest } = req.body;
+    // Joi를 사용하여 데이터 형식을 검증합니다.
+    const { error } = postUpdateSchema.validate({ title, content, password });
+    if (error) {
+        // 에러 메시지에 따라 적절한 에러 메시지를 반환합니다.
+        const errorMessage = getErrorMessage(error);
+        return res.status(412).json({ errorMessage });
+    }
+
+    // 로그인 상태를 확인합니다.
+    const { user } = req.cookies; // 'user'는 실제 쿠키 키에 따라 변경해야 합니다.
+    if (!user) {
+        return res.status(403).json({ errorMessage: '로그인이 필요한 기능입니다.' });
+    }
+
     try {
         const post = await Post.findById(req.params._postId);
         if (!post) { // 포스트아이디에 해당하는 게시글이 없음
-            return res.status(404).json({ error: '게시글 조회에 실패하였습니다.' });
+            return res.status(404).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
         }
+
         if (post.password !== password) { // 입력한 패스워드와 저장된 패스워드가 다를 경우
-            return res.status(400).json({ error: '데이터 형식이 올바르지 않습니다.' });
+            return res.status(403).json({ errorMessage: '게시글 수정의 권한이 존재하지 않습니다.' });
         }
 
         Object.entries(rest).forEach(([key, value]) => {
@@ -62,7 +115,8 @@ router.put('/:_postId', async (req, res) => {
         await post.save();
         res.status(201).json({ message: "게시글을 수정하였습니다." });
     } catch (error) {
-        res.status(400).json({ error: error.toString() });
+        console.log(error);
+        res.status(400).json({ errorMessage: '게시글 수정에 실패하였습니다.' });
     }
 });
 
@@ -70,19 +124,32 @@ router.put('/:_postId', async (req, res) => {
 // 게시글 삭제
 router.delete('/:_postId', async (req, res) => {
     const { password } = req.body;
-
+    // 쿠키를 통해 사용자를 확인합니다.
+    const { user } = req.cookies; // 'user'는 실제 쿠키 키에 따라 변경해야 합니다.
+    if (!user) {
+        return res.status(403).json({ errorMessage: '로그인이 필요한 기능입니다.' });
+    }
     try {
         const post = await Post.findById(req.params._postId);
         if (!post) { // 포스트아이디에 해당하는 게시글이 없음
-            return res.status(404).json({ error: '게시글 조회에 실패하였습니다.' });
+            return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
         }
-        if (post.password !== password) {
-            return res.status(400).json({ massage: '데이터 형식이 올바르지 않습니다.' });
+        if (post.password !== password) { // 입력한 패스워드와 저장된 패스워드가 다를 경우
+            return res.status(403).json({ errorMessage: '게시글의 삭제 권한이 존재하지 않습니다.' });
         }
+        
+        // 게시글을 삭제합니다.
+        const deleteResult = await Post.deleteOne({_id: req.params._postId });
+        if (deleteResult.deletedCount === 0) { // 삭제된 문서의 수가 0인 경우
+            return res.status(401).json({ errorMessage: '게시글이 정상적으로 삭제되지 않았습니다.' });
+        }
+        
         res.status(201).json({ message: '게시글을 삭제하였습니다.' });
     } catch (error) {
-        res.status(400).json({ error: error.toString() });
+        console.log(error);
+        res.status(400).json({ errorMessage: '게시글 삭제에 실패하였습니다.' });
     }
 });
+
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -13,39 +13,55 @@ const signupSchema = Joi.object({
         .required(),
     password: Joi.string()
         .min(4)
-        .required(),
-    passwordConfirm: Joi.ref('password')
+        .required()
+        .custom((value, helpers) => {
+            if (value.includes(helpers.state.ancestors[0].nickname)) {
+                return helpers.message('패스워드에 닉네임이 포함되어 있습니다.');
+            }
+            return value;
+        }),
+    passwordConfirm: Joi.any()
+        .equal(Joi.ref('password'))
+        .required()
+        .messages({ 'any.only': '비밀번호가 일치하지 않습니다' })
 });
+
 const loginSchema = Joi.object({
     nickname: Joi.string().required(),
     password: Joi.string().required()
 });
 
+
 // 회원 가입
 router.post('/signup', async (req, res) => {
     try {
-        const result = signupSchema.validate(req.body);
-        if (result.error) {
-            return res.status(400).json({ errorMessage: result.error.details[0].message });
-        }
+        const { error } = signupSchema.validate(req.body);
+        if (error) {
+            if (error.details[0].context.key === 'nickname') {
+                return res.status(412).send({ errorMessage: '닉네임의 형식이 일치하지 않습니다.' });
+            }
+        } 
 
         const { nickname, password } = req.body;
 
         // 중복 닉네임 확인
-        const exists = await Users.exists({ nickname: nickname });
+        const exists = await User.findOne({ where: { nickname: nickname } }); // =Users.exists()
         if (exists) {
-            return res.status(409).json({ errorMessage: '중복된 닉네임입니다.' });
+            return res.status(412).json({ errorMessage: '중복된 닉네임입니다.' });
         }
 
         const hashedPassword = bcrypt.hashSync(password, 10);
-        const user = new Users({ nickname, password: hashedPassword });
-        await user.save();
+        const user = await User.create({ // = new Users()
+            nickname: nickname, 
+            password: hashedPassword
+        });
 
         res.status(201).json({});
     } catch (error) {
-        res.status(400).json({ errorMessage: '회원 가입에 실패하였습니다.' });
+        res.status(400).json({ errorMessage: '요청한 데이터 형식이 올바르지 않습니다.' });
     }
 });
+
 
 // 로그인
 router.post('/login', async (req, res) => {
